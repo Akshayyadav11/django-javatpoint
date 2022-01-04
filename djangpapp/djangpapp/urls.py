ed87150552fbd64b225f19c50ade0c2f962d334e
Akshay <akshayy976@gmail.com>
2022-01-04 10:43:06 +0530

    models

--- djangpapp/djangpapp/urls.py
+++ djangpapp/djangpapp/urls.py
@@ -20,5 +20,6 @@ from myapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello)
+    path('hello/', views.hello),
+    path('index/', views.index)
 ]
--- djangpapp/myapp/views.py
+++ djangpapp/myapp/views.py
@@ -1,7 +1,18 @@
-from django.shortcuts import render
-
+from django.template import loader
+import datetime
 # Create your views here.
 from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse('<h2>Hello World</h2>')
+
+def index(request):  
+    # now = datetime.datetime.now()  
+    # html = "<html><body><h3>Now time is %s.</h3></body></html>" % now  
+    # return HttpResponse(html)
+    name = {  
+        'student':'rahul'  
+    }  
+
+    template = loader.get_template('index.html') # getting our template  
+    return HttpResponse(template.render(name)) 
