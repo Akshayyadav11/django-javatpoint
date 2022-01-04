6c1aac640330493e3ec63ef3ce7c3791476fbe17
Akshay <akshayy976@gmail.com>
2022-01-04 14:22:19 +0530

    models

--- djangpapp/myapp/views.py
+++ djangpapp/myapp/views.py
@@ -3,6 +3,7 @@ import datetime
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
+from myapp.forms.form import EmpForm
 
 def hello(request):
     return HttpResponse('<h2>Hello World</h2>')
@@ -11,11 +12,11 @@ def index(request):
     # now = datetime.datetime.now()  
     # html = "<html><body><h3>Now time is %s.</h3></body></html>" % now  
     # return HttpResponse(html)
-    name = {  
-        'student':'rahul'  
-    }  
-
+    # name = {  
+    #     'student':'rahul'  
+    # }  
+    stdform = EmpForm()
     template = loader.get_template('index.html') # getting our template  
-    return HttpResponse(template.render(name)) 
+    return HttpResponse(template.render({'details':stdform}))
 
     # return render(request, 'index.html')
